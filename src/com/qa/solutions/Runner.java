49cb07e742c3633522e860075f44889154dbac9f
Ash <ashsiva4@hotmail.co.uk>
2021-02-11 19:08:16 +0000

    AS: numbers sample solution added

--- src/com/qa/solutions/Runner.java
+++ src/com/qa/solutions/Runner.java
@@ -3,7 +3,13 @@ package com.qa.solutions;
 public class Runner {
 	public static void main(String[] args) {
 		
+		NumbersEx num = new NumbersEx();
+		System.out.println(num.add2Dig(23));
+		System.out.println(num.stringRep(4342));
+		num.one2hund();
+
 		CoinsEx test = new CoinsEx();
 		test.change(4.58, 20);
+		
 	}
 }
